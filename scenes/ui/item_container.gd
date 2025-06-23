15ad6847f7b98b4a56bd316270f78f9dd9a67546
brenwickgit <brenwick02@gmail.com>
2025-06-22 20:56:59 -0700

    fixed ui bug with inventory

--- scenes/ui/item_container.gd
+++ scenes/ui/item_container.gd
@@ -1,25 +1,37 @@
-extends MarginContainer
+extends PanelContainer
 
 signal placeable_selected(placeable: RPlaceable)
 
-@onready var item_btn: Button = $PanelContainer/ItemBtn
-@onready var panel_container: PanelContainer = $PanelContainer
-@onready var key_label: Label = $PanelContainer/MarginContainer/KeyLabel
+@onready var item_btn: Button = $MarginContainer/Panel/ItemBtn
+@onready var margin_container: MarginContainer = $MarginContainer
+@onready var key_label: Label = $MarginContainer/Panel/KeyLabel
+@onready var circle_panel: Panel = $CirclePanel
 
 @export var index: int
 @export var placeable: RPlaceable
+@export var item_count: int = 0
 
 func _hide_ui() -> void:
 	item_btn.visible = false
-
+	circle_panel.visible = false
+	
 func _show_ui() -> void:
 	item_btn.visible = true
+	circle_panel.visible = true
 
 func _update_ui() -> void:
 	var icon: TextureRect = placeable.icon_scene.instantiate()
 	item_btn.pressed.connect(on_btn_pressed)
 	item_btn.add_child(icon)
-
+	if item_count == 0:
+		item_btn.disabled = true
+		item_btn.mouse_filter = Control.MOUSE_FILTER_IGNORE 
+		item_btn.focus_mode = Control.FOCUS_NONE
+	else:
+		item_btn.disabled = false
+		item_btn.mouse_filter = Control.MOUSE_FILTER_STOP 
+		item_btn.focus_mode = Control.FOCUS_ALL
+	
 func _ready() -> void:
 	key_label.text = str(index)
 	if placeable: _update_ui()
